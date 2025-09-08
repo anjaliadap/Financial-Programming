2b5ceedfb9eaabf11e4ad0ad46a143f08414588e
anjaliadap <aa619@fordham.edu>
2025-09-08 15:45:34 -0400

    newFiles

---
+++ HW2/homework2.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    double x=3.0;
+    // Problem 1.1
+    double y_1;
+    
+    // Problem 1.2
+    double y_2;
+    
+
+    // Problem 2.1
+    double a_0=1; 
+    double a_1=-2;
+    double a_2=1;
+    double a_3=1;
+    double y_3; 
+
+    // Problem 2.2
+    double y_4;
+
+
+    // Problem 3.1
+    double K=100;
+    double S=110;
+    double call;
+    
+    // Problem 3.2
+    double put;
+    
+    // Problem 3.3
+    bool is_call=true;
+    
+    // Problem 3.4
+    double call2;
+    double put2;
+   
+
+    // Problem 4.1
+    double T=10;
+    double r=0.04;
+    double c=0.05;
+    double bond_price;
+    
+    // Problem 4.2
+    double bond_price2; 
+    
+}
