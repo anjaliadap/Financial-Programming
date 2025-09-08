a2666f006a9014be4ed3534fc49f2fb50d4324a1
anjaliadap <aa619@fordham.edu>
2025-09-08 17:13:56 -0400

    solution

--- HW2/homework2.cpp
+++ HW2/homework2.cpp
@@ -1,15 +1,21 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <print>
+using namespace std;
 
 int main() {
     double x=3.0;
+   
     // Problem 1.1
     double y_1;
+    y_1 = 2 + (3*x) + (4*pow(x,2));
+    println("y_1 = {}", y_1, "\n");
     
     // Problem 1.2
     double y_2;
-    
+    y_2 = (x/(1+x)) * exp(-2*x);
+    println("y_2 = {}", y_2, "\n");
 
     // Problem 2.1
     double a_0=1; 
@@ -17,34 +23,66 @@ int main() {
     double a_2=1;
     double a_3=1;
     double y_3; 
+    y_3 = a_0 + (a_1*x) + (a_2*pow(x, 2)) + (a_3*pow(x, 3));
+    println("y_3 = {}", y_3, "\n");
 
     // Problem 2.2
     double y_4;
+    y_4 = (a_0 + (a_1*x)) / (a_2 + (a_3*x));
+    println("y_4 = {}", y_4, "\n");
 
 
     // Problem 3.1
     double K=100;
     double S=110;
     double call;
+
+    if (S > K) { call = S - K; }
+    else { call = 0; }
+    println("call = {}", call, "\n");
+
     
     // Problem 3.2
     double put;
+    if (S < K) { put = K - S; }
+    else { put = 0; }
+    println("put = {}", put, "\n");
     
     // Problem 3.3
     bool is_call=true;
+
+    if (is_call && (S > K)) {println("call = {}", (S - K), "\n");}
+    else if (!is_call && (S < K)) {println("put = {}", (K - S), "\n");}
+    else { println("The option value is {}", 0, "\n");}
     
     // Problem 3.4
     double call2;
     double put2;
-   
+
+    call2 = max((S - K), 0.0);
+    put2 = max((K - S), 0.0);
+
+    println("call = {}", call2, "\n");
+    println("put = {},", put2, "\n");
 
     // Problem 4.1
     double T=10;
     double r=0.04;
     double c=0.05;
-    double bond_price;
-    
+    double bond_price = 0;
+
+    int i = 0;
+    while (i < T) {
+        bond_price += (c * exp(-r * T)) + exp(-r * T);
+        i++;
+    }
+    println("bond price (while loop) = {}", bond_price, "\n");
+
     // Problem 4.2
-    double bond_price2; 
-    
+    double bond_price2 = 0; 
+    int j; 
+    for(j = 0; j < T; j++) {
+        bond_price2 += (c * exp(-r * T)) + exp(-r * T);
+    }
+    println("bond price (for loop) = {}", bond_price2);
 }
